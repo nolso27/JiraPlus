8eaf2ec77d14e7f3201a6ccb2205db901369ac9c
Nate Olson <nate.ols27@gmail.com>
2024-02-02 17:04:17 -0800

    bellllll

--- background.js
+++ background.js
@@ -1,96 +1,49 @@
-// Function to listen for notify message
-function listenForNotify() {
-    console.log('BG: Listening for notify message');
-    chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-        if (message.type === 'notify') {
-            console.log('BG: Notify button was pressed');
-            var notifyProperties = { modeActive: null, notifyTab: null};
-            notifyProperties.modeActive = true;
-            notifyProperties.notifyTab = message.data;
-            console.log('BG: Notify properties', notifyProperties);
-            chrome.storage.local.set({ "notifyMode": notifyProperties });
-            console.log(message.data)
-            chrome.browserAction.setIcon({ path: 'icon.png', tabId: message.data})
-            notifyMode();
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) { // 
+    if (message.type === 'notify') {
+        console.log('BG: Notify button was pressed');
+        var notifyProperties = { modeActive: null, notifyTab: null};
+        notifyProperties.modeActive = true;
+        notifyProperties.notifyTab = message.data;
+        console.log('BG: Notify properties', notifyProperties);
+        chrome.storage.local.set({ "notifyMode": notifyProperties });
+        console.log(message.data)
+        try {
+            chrome.browserAction.setIcon({ path: 'icon.png', tabId: message.data});
+        } catch (e) {
+            console.error(e);
         }
-    });
-}
-
-// Function to handle notify mode
-function notifyMode() {
-    let exclusionAmount = 0; // Amount of tickets to exclude from the new ticket notifier. I.e. if amount is 3, 4 tickets must be in to notify
-    console.log('BG: Entered notify mode');
-    // Listen for resultfound message
-    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-        if (message.type === 'resultfound') {
-            let resultLength = message.data;
-            console.log(`BG: Result found! ${resultLength} tickets`);
-            chrome.storage.local.get(["exclusion"], function (result) {
-                if (result.exclusion) {
-                    console.log('BG: Retrieved exclusion:', result.exclusion);
-                    exclusionAmount = result.exclusion;
-                    if (resultLength > exclusionAmount) {
-                        console.log(`BG: ${resultLength} tickets found, and ${exclusionAmount} ticket(s) excluded. Creating notification and playing sound.`);
-                        createNotification();
-                        playSound();
-                    }
-                }
-            });
-        }
-    });
-}
-
-// Function to create notification
-function createNotification() {
-    let opt = {
-        type: "basic",
-        title: "Jira+",
-        message: "A new ticket is in!",
-        iconUrl: "icon.png"
-    };
-    chrome.notifications.create("notificationName", opt, function () {
-        console.log('BG: Notification created');
-        // Placeholder comment: Handle notification creation callback if needed
-    });
-}
-
-// Function to play sound
-async function playSound(source = 'notif1.mp3') {
-    // Retrieve stored volume value from long term storage
-    chrome.storage.local.get(["volume"], function (result) {
-        let volume = result.volume || 0.5; // Set default volume to 0.5 if not found in storage
-        console.log(`BG: Volume set to ${volume}`);
-        // Create the offscreen document if it doesn't already exist
-        createOffscreen().then(() => {
-            console.log('BG: Sending message to play sound');
-            chrome.runtime.sendMessage({ play: { source, volume } });
-        });
-    });
-}
-
-// Create the offscreen document if it doesn't already exist
-async function createOffscreen() {
-    if (await chrome.offscreen.hasDocument()) return;
-    console.log('BG: Creating offscreen document');
-    await chrome.offscreen.createDocument({
-        url: 'offscreen.html',
-        reasons: ['AUDIO_PLAYBACK'],
-        justification: 'testing' // Placeholder comment: Provide specific details for using the API
-    });
-}
-
-// Check the value of notifyMode in storage
-chrome.storage.local.get(['notifyMode']).then((result) => {
-    console.log('BG: Notify mode data retrieved from storage:', result.notifyMode);
-    if (typeof(result === "undefined")) {
-        console.log('BG: Notify mode not found in storage. Initiating listener.');
-        listenForNotify();
-    }
-    if (result.notifyMode.modeActive === false || result.notifyMode.modeActive === null) {
-        listenForNotify();
-    }
-    if (result.notifyMode.modeActive) {
-        console.log('BG: Notify mode active. Initiating notifyMode.');
-        notifyMode();
     }
 });
+
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.type === 'resultfound') {
+        console.log("TEST")
+        console.log(`BG: Result found!`);
+
+        chrome.tabs.update(result.notifyMode.tabId, {active: true});
+        // createNotification();
+
+        // playSound();
+    }
+});
+
+
+// Function to create notification
+// function createNotification() {
+//     let opt = {
+//         type: "basic",
+//         title: "Jira+",
+//         message: "A new ticket is in!",
+//         iconUrl: "icon.png"
+//     };
+//     chrome.notifications.create("notificationName", opt, function () {
+//         console.log('BG: Notification created');
+//         // Placeholder comment: Handle notification creation callback if needed
+//     });
+// }
+
+// Create the offscreen document if it doesn't already exist
+
+
+
--- content.js
+++ content.js
@@ -9,75 +9,39 @@ if (document.readyState !== 'complete') {
     jiraSearch();
 }
 
+
+function jiraSearch() {
+    console.log('Window loaded');
+    chrome.storage.local.get(['notifyMode']).then((result) => {
+        console.log("Notify mode active?", result.notifyMode.modeActive);
+        if (result.notifyMode.modeActive) {
+            try{
+                updateHTML();
+            } catch (e) {
+                console.error("Could not finish updating page content: ", e)
+            }
+            
+            notifyMode();
+        } else {
+            console.log('Notify mode not active. Redirecting to default page...');
+        }
+    });
+}
+
+
+
+
 function notifyMode() {
     console.log('Entering notifyMode function');
+
+    // Variable to track if notification has already been sent
+    var notificationSent = false;
 
     var timeoutId;
     var observer;
     var refreshInterval = 30000; // How long it takes for it to recheck for new tickets
-    var originalTitle = document.title;
-    var notifyModeTitle = 'Tab is in Notify Mode';
-    var toggleTitle = false; // Flag to toggle between original and notify mode titles
-    var toggleTitleInterval; // Variable to store the interval ID for title toggling
-    
 
-    function toggleTitleText() {
-        document.title = toggleTitle ? originalTitle : notifyModeTitle;
-        toggleTitle = !toggleTitle;
-    }
-
-    function processSearchResults() {
-        console.log('Processing search results...');
-
-        // Process the search results here
-        var liElements = document.querySelectorAll('.issue-list li');
-        chrome.runtime.sendMessage({ type: 'resultfound', data: liElements.length });
-
-        chrome.storage.local.get(["exclusion"], function (result) {
-            if (result.exclusion) {
-                console.log('Retrieved exclusion:', result.exclusion);
-                exclusionAmount = result.exclusion;
-                if (liElements.length > exclusionAmount) {
-                    console.log(`New ticket found (${liElements.length} > ${exclusionAmount})`);
-                    document.title = 'ALERT: New Ticket Found!';
-
-                    // Prompt the user with a popup
-                    var userResponse = false;
-
-                    function closePrompt() {
-                        clearInterval(promptInterval);
-
-                        if (userResponse) {
-                            chrome.storage.local.set({ "notifyMode": { modeActive: false } });
-                            window.location.href = document.getElementsByClassName('splitview-issue-link')[0].href;
-                        } else {
-                            console.log('User declined to continue or the timeout expired.');
-                        }
-                    }
-
-                    // Display the prompt and handle user response
-                    var promptInterval = setInterval(function () {
-                        userResponse = window.confirm('A new ticket has been found. Would you like to open the latest ticket? NOTE: This will end notify mode.');
-                        closePrompt();
-                    }, 100);
-
-                    // Clear the title toggling interval if a new ticket is found
-                    // bell.svg
-
-                    clearInterval(toggleTitleInterval);
-                } else {
-                    console.log('No new tickets found');
-                    // Reset the toggleTitle flag if no new ticket is found
-                    toggleTitle = false;
-                    // Clear the title toggling interval
-                    clearInterval(toggleTitleInterval);
-                    // Start the interval for title toggling if not already started
-                    toggleTitleInterval = setInterval(toggleTitleText, 2000);
-                }
-            }
-        });
-    }
-
+    // Initialize the observer here
     observer = new MutationObserver(function (mutations) {
         mutations.forEach(function (mutation) {
             if (mutation.addedNodes.length > 0) {
@@ -92,25 +56,72 @@ function notifyMode() {
     if (elements.length > 0) {
         if (document.getElementsByClassName('issue-list')[0].getElementsByTagName('li').length === 0) {
             observer.observe(elements[0], { childList: true, subtree: true });
-            // Clear the title toggling interval
-            clearInterval(toggleTitleInterval);
-            // Start the interval for title toggling if not already started
-            toggleTitleInterval = setInterval(toggleTitleText, 2000);
         } else {
             console.log('Issue list already populated');
             processSearchResults();
         }
     } else {
-        
-        
         console.log('No results container found');
     }
+
+    function processSearchResults() {
+        
+        console.log('Processing search results...');
+        // Process the search results here
+        var liElements = document.querySelectorAll('.issue-list li');
+
+        chrome.storage.local.get(["exclusion"], function (result) {
+            if (result.exclusion) {
+                console.log('Retrieved exclusion:', result.exclusion);
+                exclusionAmount = result.exclusion;
+                if (liElements.length > exclusionAmount && !notificationSent) {
+                    console.log(`New ticket found (${liElements.length} > ${exclusionAmount})`);
+
+                    // Send a message to the background script
+                    chrome.runtime.sendMessage({ type: 'resultfound' });
+                    var audio = new Audio(chrome.runtime.getURL('notif1.mp3 '));
+                    audio.play();
+
+                    // Set the notificationSent flag to true
+                    notificationSent = true;
+
+                    // Prompt the user with a popup
+                    var userResponse = false;
+                    
+                    // Prompt the user with a popup
+                    var userResponse = false;
+
+                    function closePrompt() {
+                        clearInterval(promptInterval);
+
+                        if (userResponse) {
+                            // Set the notificationSent flag to true
+
+                            chrome.storage.local.set({ "notifyMode": { modeActive: false } });
+                            window.location.href = document.getElementsByClassName('splitview-issue-link')[0].href;
+                        } else {
+                            console.log('User declined to continue or the timeout expired.');
+                        }
+                    }
+
+                    // Display the prompt and handle user response
+                    var promptInterval = setInterval(function () {
+                        userResponse = window.confirm('A new ticket has been found. Would you like to open the latest ticket? NOTE: This will end notify mode.');
+                        closePrompt();
+                    }, 100);
+                } else {
+                    console.log('No new tickets found or notification already sent');
+                }
+            }
+        });
+    }
+
+    
 
     // Set the refresh interval outside the if condition
     setInterval(function () {
         console.log('Refreshing page...');
         // Clear all intervals and timeouts
-        clearInterval(toggleTitleInterval);
         clearTimeout(timeoutId);
         location.reload(true);
     }, refreshInterval);
@@ -121,40 +132,12 @@ function notifyMode() {
             console.log('Page is visible, triggering search results processing...');
             // Page is visible, trigger the search results processing
             processSearchResults();
-        }
-    });
-}
-
-function jiraSearch() {
-    console.log('Window loaded');
-    chrome.storage.local.get(['notifyMode']).then((result) => {
-        console.log("Notify mode active?", result.notifyMode.modeActive);
-        if (result.notifyMode.modeActive) {
-            try{
-                updateHTML();
-            } catch {}
-            
-            notifyMode();
         } else {
-            // window.location.href = 'https://jira.benco.com/issues/?jql=project = BEN AND status = Untriaged AND assignee in (EMPTY)'
-            console.log('Notify mode not active. Redirecting to default page...');
+            console.error("Could not process result: Page did not load in alloted time.")
         }
     });
 }
 
-function updateHTML() {
-    document.querySelector(".no-results.no-results-message").style.backgroundImage = `url(${chrome.runtime.getURL('bell.svg')})`;
-    document.getElementById("header").remove();
-    document.getElementById("navigator-sidebar").remove();
-    document.getElementsByClassName("issue-search-header")[0].remove();
-    document.getElementsByClassName("navigator-sidebar collapsed")[0].remove();
-    document.querySelector(".no-results.no-results-message h2").innerHTML = "You are in notify mode. You will hear a ding when we find a ticket.";
-    document.querySelector(".no-results.no-results-message p").innerHTML = 'Nothing to see here.';
-    document.querySelector(".navigator-content.empty-results").style.display = "flex";
-    document.querySelector(".navigator-content.empty-results").style.alignItems = "center";
-    document.querySelector(".navigator-content.empty-results").style.justifyContent = "center";
-    document.querySelectorAll('[rel="shortcut icon"]')[0].href = chrome.runtime.getURL('icon.png');
-}
 
 function jiraBrowse() {
     // Get the div with the ID customfield-panel-1
@@ -163,6 +146,32 @@ function jiraBrowse() {
     var div1 = document.querySelector('.module.toggle-wrap');
     div1.insertAdjacentHTML('afterend', `<div class="module toggle-wrap"><div class="mod-header"><button class="aui-button toggle-title" aria-label="Jira+" aria-controls="Jira+-module" aria-expanded="true" resolved=""><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14"><g fill="none" fill-rule="evenodd"><path d="M3.29175 4.793c-.389.392-.389 1.027 0 1.419l2.939 2.965c.218.215.5.322.779.322s.556-.107.769-.322l2.93-2.955c.388-.392.388-1.027 0-1.419-.389-.392-1.018-.392-1.406 0l-2.298 2.317-2.307-2.327c-.194-.195-.449-.293-.703-.293-.255 0-.51.098-.703.293z" fill="#344563"></path></g></svg></button><h4 class="toggle-title" id="details-module-label">Jira+</h4><ul class="ops"></ul></div><div class= "mod-content"><div id="customfieldmodule"><div class="aui-tabs horizontal-tabs" id="customfield-tabs" role="application"><div class=" active-pane"> <ul class="property-list"> <li id="rowForcustomfield_12210" class="item"> <div class="wrap"> <strong title="Account Number" class="name"> <label for="customfield_12210">Ticket History:</label> </strong> <div data-fieldtype="textfield" class="value"> <a id="ticket-history" href='https://jira.benco.com/issues/?jql=project = BEN AND "Account Number" ~ "${acctNum}" ORDER BY created DESC'>Click Here</a> </div> </div> </li> <li id="rowForcustomfield_12210" class="item"> <div class="wrap"> <strong title="Account Number" class="name"> <label for="customfield_12210">Placeholder</label> </strong> <div data-fieldtype="textfield" class="value"> placeholder </div> </div> </li> <li id="rowForcustomfield_12210" class="item"> <div class="wrap"> <strong title="Account Number" class="name"> <label for="customfield_12210">Placeholder</label> </strong> <div data-fieldtype="textfield" class="value"> placeholder </div> </div> </li> <li id="rowForcustomfield_12210" class="item"> <div class="wrap"> <strong title="Account Number" class="name"> <label for="customfield_12210">Place holder</label> </strong> <div data-fieldtype="textfield" class="value"> placeholder </div> </div> </li>   </ul> </div></div></div></div></div>`);
     document.getElementById('ticket-history').onclick = function(){chrome.storage.local.set({ "notifyMode": { modeActive: false } });}
+}
+
+function updateHTML() {
+    document.getElementsByClassName("issue-search-header")[0].remove();
+    document.getElementById("header").remove();
+    document.getElementById("navigator-sidebar").remove();
+    document.getElementsByClassName("navigator-sidebar collapsed")[0].remove();
 
 
+
+    var results = document.getElementsByClassName("simple-issue-list"); // Check if there are search results
+    console.log(results.length)
+    if (results.length === 0) {
+        document.querySelector(".no-results.no-results-message").style.backgroundImage = `url(${chrome.runtime.getURL('bell.png')})`;
+        document.querySelector(".no-results.no-results-message h2").innerHTML = "You are in notify mode. You will hear a ding when we find a ticket.";
+        document.querySelector(".no-results.no-results-message p").innerHTML = 'Nothing to see here.';
+        document.querySelector(".navigator-content.empty-results").style.display = "flex";
+        document.querySelector(".navigator-content.empty-results").style.alignItems = "center";
+        document.querySelector(".navigator-content.empty-results").style.justifyContent = "center";
+        document.querySelectorAll('[rel="shortcut icon"]')[0].href = chrome.runtime.getURL('icon.png')
+    } else {
+        document.getElementsByClassName("inline-issue-create-container")[0].remove();
+        document.getElementsByClassName("pagination-view")[0].remove();
+        document.getElementsByClassName("detail-panel")[0].remove();
+        document.getElementsByClassName("ui-sidebar")[0].remove();
+        document.getElementsByClassName("search-results")[0].insertAdjacentHTML("afterend", '<div class="no-results no-results-message" style="background-image: url(&quot;chrome-extension://nfelnemkdibpebjbbmeloldodgelgiel/bell.gif&quot;); margin-top: 10%;"><h2>We found a ticket!!</h2><p class="no-results-hint">Time to wake up.</p></div>')
+        console.log("pushing")
+    }
 }
